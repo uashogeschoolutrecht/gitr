05f80c828f9a2a9e95fce639348c9fb402c2a250
maddocent <marc.teunis@hu.nl>
2018-11-21 09:21:48 +0100

    minor bug fix

--- R/set_git_user.R
+++ R/set_git_user.R
@@ -1,7 +1,6 @@
 #' @title A function to easily set the git user.name and email in R
-#' 
-email = "marc.teunis@hu.nl"
-username = "maddocent"
+#'
+#' @export
 
 set_git_user <- function(email, username){
 
@@ -9,24 +8,21 @@ set_git_user <- function(email, username){
   system(paste("git config --global user.name", username))
 
   x <- system("git config --list", intern = TRUE)
-  
+
   length_email_set <- length(x[1])
   length_user_set <- length(x[2])
 
   if(length(length_email_set) > 0) {
     message(paste ("The email for your git account has been set to:",
-                   email)) 
+                   email))
     }
 
   if(length(length_user_set) > 0) {
       message(paste ("The user.name for your git account has been set to:",
                      username))
-    }  
-            
-  }
-  
-  
-  
+    }
 
-set_git_user(email = email,
-             username = username)
+  }
+
+
+
